d6f951b27ff826e8a120d1bda9ffdd6f5dce8154
Paul Nakonieczny <nakonieczny.paul@gmail.com>
2021-08-26 01:46:49 +0000

    Basic ejs landing page

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const indexRouter = require('./routes/indexRouter');
+
+const PORT = 8000;
+const HOST = '0.0.0.0';
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Hi!')
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
